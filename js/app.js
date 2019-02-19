6e0aa52aff3f1829ae4b614d12fd1c3912616b85
Kushal Sinha Roy <kushalr@amazon.com>
2019-02-19 00:00:35 -0800

    Add introduction page, and minor modification to html and JS

--- js/app.js
+++ js/app.js
@@ -9,7 +9,8 @@ the end user is given how many they scored correct.
 var firstName = prompt("What is your First name?");
 var lastName = prompt("What is your Last Name?");
 
-//get the counter going for correct answers.
+//variables for number of questions and correct answers.
+var numberOfQuestion = 0;
 var correctAnswers = 0;
 
 //greet the user and check if interested in playing the game.
@@ -26,51 +27,63 @@ if(guessingGameFlag.toUpperCase() === 'Y' || guessingGameFlag.toUpperCase() === 
 
     //1st question
     var homeCountry = prompt('Is my home country India?');
+    numberOfQuestion++;
     console.log('Home Country:', homeCountry);
 
     if(homeCountry.toUpperCase() === 'Y' || homeCountry.toUpperCase() === 'YES') {
         //Increment correct answer counter 
         correctAnswers++;
+        alert("Correct Answer!!!");
 
         //2nd Question
         var homeCountryCity = prompt("Guess my home city - Kolkata, Delhi, Mumbai, Goa ?");
+        numberOfQuestion++;
         console.log('Home City:', homeCountryCity);
 
         if (homeCountryCity.toUpperCase() === "KOLKATA") {
             correctAnswers++;
+            alert("Correct Answer!!!");
         }
     }
 
     //3rd Question
     var favoritePastTime = prompt('Is playing video games my favorite past time?');
+    numberOfQuestion++;
     console.log('Favorite Past Time', favoritePastTime);
 
     if(favoritePastTime.toUpperCase() === 'YES' || favoritePastTime.toUpperCase() === 'Y') {
         correctAnswers++;
+        alert("Correct Answer!!!");
 
         //4th Question
         var favoriteVideoGame = prompt('Is FIFA my favorite video game?');
+        numberOfQuestion++;
         console.log('Favorite video game', favoriteVideoGame);
 
         if(favoriteVideoGame.toUpperCase() === 'Y' || favoriteVideoGame.toUpperCase() === 'YES') {
             correctAnswers++;
+            alert("Correct Answer!!!");
         }
     }
 
-    alert("Ok. You have another 3 chances to guess my favorite number. HINT: It's a multiple of 3")
+    alert("Ok. You have another 3 chances to guess my favorite number. HINT: It's a multiple of 3");
+    numberOfQuestion++;
 
     var attempts = 0;
     while(attempts <= 2) {
-        
+
         //5th Question
         var favoriteNumber = prompt('What is my favorite number?');
+        console.log('Favorite Number Attempt ', attempts, ' is ', favoriteNumber);
+
         if(parseInt(favoriteNumber) === 9) {
             correctAnswers++;
+            alert("Correct Answer!!!");
             break;
         }
         attempts++;
     }
-    alert('You answered  ' + correctAnswers + ' out of 5 questions.');
+    alert('You answered  ' + correctAnswers + ' out of ' + numberOfQuestion +' questions.');
 } else {
     alert("Thanks and appreciate your time searching for this guessing game!!!");
 }
